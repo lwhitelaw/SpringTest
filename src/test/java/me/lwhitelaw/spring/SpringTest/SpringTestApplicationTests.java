76eb0ecde221ef12aed8120ab9a67de2da72d1e5
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-02-18 21:07:40 -0500

    Spring Web init

---
+++ src/test/java/me/lwhitelaw/spring/SpringTest/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.lwhitelaw.spring.SpringTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
