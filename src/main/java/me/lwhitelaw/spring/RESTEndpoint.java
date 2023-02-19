5eca65505370b11f928063828316cdcff418eedb
lwhitelaw <48133276+lwhitelaw@users.noreply.github.com>
2023-02-18 22:14:53 -0500

    Try a path variable parameter

--- src/main/java/me/lwhitelaw/spring/RESTEndpoint.java
+++ src/main/java/me/lwhitelaw/spring/RESTEndpoint.java
@@ -1,6 +1,7 @@
 package me.lwhitelaw.spring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,10 @@ public class RESTEndpoint {
 	@GetMapping("/string")
 	public String string(@RequestParam("value") String value) {
 		return "Your string is " + value;
+	}
+	
+	@GetMapping("/read/{sha3}")
+	public String read(@PathVariable String sha3) {
+		return "Requested " + sha3;
 	}
 }
